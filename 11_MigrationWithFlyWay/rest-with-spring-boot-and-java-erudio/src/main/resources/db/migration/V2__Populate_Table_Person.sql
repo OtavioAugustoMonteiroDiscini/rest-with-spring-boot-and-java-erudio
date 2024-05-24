6e07ef2c4d0602f7e45bd6f3eb75e41e57c4cf2e
OtavioAugustoMonteiroDiscini <otavio2005discini@gmail.com>
2024-05-24 00:16:39 -0300

    JSON Serializtion

---
+++ 11_MigrationWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` VALUES (1,'São Paulo','Ayrton','male','Sena'),
+(2,'Ourinhos - São Paulo - Brasil ','Otávio Augusto','Male','Monteiro Discini'),
+(6,'São Paulo - SP','Juan','male','Marini'),
+(7,'Rio de Janeiro - RJ','Juan','male','Marini Prisma'),
+(9,'Kentucky - US','Muhammad','Male','Ali'),
+(10,'Mvezo - South Africa','Nelson','Male','Mandela'),
+(11,'Smildian - Croatia','Nikolas','Male','Tesla');
+
+
